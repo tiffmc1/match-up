b3ec0c02b2c915e7181c81a5fd62417d6fb1b9b6
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-04-30 14:35:21 -0400

    index.js: cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-// import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
-
-// const App = () => {
-// 	return <h1>Hello World!</h1>;
-// };
-
-//ReactDOM.render(<App />, document.getElementById("app"));
-
 import { createRoot } from "react-dom/client";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
+
 root.render(<App />);
